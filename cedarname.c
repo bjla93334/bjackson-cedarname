261150da822b7159a54b25ff591d895669981294
Bill Jackson <bjla93334@gmail.com>
2026-07-15 02:26:30 -0700

    exit code for real this time

--- cedarname.c
+++ cedarname.c
@@ -8,13 +8,16 @@
 #include "pfs.h"
 
 int main(int argc, char *argv[]) {
+    int rc = 0;
     for (int i = 1; i < argc; ++i) {
         char *pfs_path = argv[i];
         char *p = pfs_TranslateName(pfs_path);
 	if (p == NULL) {
 	    fprintf(stderr, "%s : %s\n", pfs_path, pfs_errorMsg);
+            rc = 1;
 	    continue;
 	}
 	printf("%s\n", p);
     }
+    return rc;
 }
